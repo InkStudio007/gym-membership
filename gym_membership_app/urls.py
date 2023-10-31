1175eafe1b49c0f96f0b935a96f3c4a862d528c2
Ink <inkstudio007@gmail.com>
2023-10-31 21:20:36 +0330

    user-authentication

--- gym_membership_app/urls.py
+++ gym_membership_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import MembershipCreate, MembershipDelete, MembershipView, MembershipRenewal
+from .views import MembershipCreate, MembershipDelete, MembershipView, MembershipRenewal, Login, Register
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', Register.as_view(), name='register'),
+
     path('', views.membership_list, name='membership_list'),
     path('add-member', MembershipCreate.as_view(), name='membership_create'),
     path('delete-member/<int:pk>/', MembershipDelete.as_view(), name='membership_delete'),
